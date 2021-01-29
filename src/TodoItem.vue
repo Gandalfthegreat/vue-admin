43a95a7dcd5b7b57e9819ce04e237a33559e4ac2
Gandalfthegreat <785918558@qq.com>
2021-01-30 07:51:41 +0800

    feat: temp save

---
+++ src/TodoItem.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.todo-list li .editing .view {
+  display: none;
+}
+.todo-list li .editing .edit {
+  display: block;
+  width: 506px;
+  padding: 12px 16px;
+  margin: 0 0 0 43px;
+}
+</style>
