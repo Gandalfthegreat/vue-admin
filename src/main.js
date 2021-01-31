0c00e02bca48d27713099d083b62da79bd886ad7
wangchao11 <wangchao11@kuaishou.com>
2021-01-31 11:06:20 +0800

    feat: temp router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Todo from "./pages/Todo.vue";
+import VueRouter from "vue-router";
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
-Vue.config.productionTip = false
-
+const routers = [
+  { path: "Todo", component: Todo, name: "Todo" },
+  {
+    path: "*",
+    redirect: { name: "Todo" }
+  }
+];
+const router = new VueRouter({
+  routers
+});
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router
+}).$mount("#app");
---
+++ src/pages/Todo.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="app">
+    <div class="todoapp">
+      <header class="header">
+        <h1>TodoList</h1>
+        <!-- 点击回车，新建一个todo项-->
+        <input class="new-todo" @keydown.enter="addTodo" v-model="newTodo" />
+      </header>
+      <section class="main">
+        <ul class="todo-list">
+          <li
+            class="todo"
+            :class="{completed: todo.completed,editing: todo.id === editedTodo.id}"
+            v-for="todo in todos"
+            :key="todo.id"
+          >
+            <!-- 1.checkbox选中做完的项 2.双击编辑 3.删除-->
+            <todo-item
+              :title.sync="todo.title"
+              :completed.sync="todo.completed"
+              @delete="removeItem(todo)"
+            ></todo-item>
+          </li>
+        </ul>
+      </section>
+      <footer class="footer" v-show="todos.length">
+        <span
+          class="todo-count"
+        >{{ remaining }} {{ remaining | pluralize }} / {{ todos.length }} {{ remaining | total }}</span>
+        <button class="clear-completed" @click="removeCompleted">clear</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+let id = 1;
+import TodoItem from "../TodoItem";
+export default {
+  data() {
+    return {
+      todos: [],
+      newTodo: "",
+      editedTodo: {}
+    };
+  },
+  components: {
+    "todo-item": TodoItem
+  },
+  computed: {
+    remaining() {
+      return this.todos.filter(x => !x.completed).length;
+    }
+  },
+  filters: {
+    pluralize(num) {
+      return num > 1 ? "items" : "item";
+    },
+    total(num) {
+      return num > 3 ? "total" : "";
+    }
+  },
+  methods: {
+    addTodo() {
+      if (!this.newTodo) {
+        return;
+      }
+      this.todos.unshift({
+        id: id++,
+        title: this.newTodo,
+        completed: false
+      });
+      this.newTodo = "";
+    },
+    removeItem(todo) {
+      // 找到待删除项index
+      const toRemoveIndex = this.todos.findIndex(item => item.id === todo.id);
+      this.todos.splice(toRemoveIndex, 1);
+    },
+    removeCompleted() {
+      this.todos = this.todos.filter(x => !x.completed);
+    }
+  }
+};
+</script>
+
+<style>
+@import "https://unpkg.com/todomvc-app-css@2.1.0/index.css";
+</style>
