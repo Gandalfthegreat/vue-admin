43b5a7bb04b6d2821f5fd3fc479f92913f1e49b7
Gandalfthegreat <785918558@qq.com>
2021-01-31 08:40:28 +0800

    todo query

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import Todo from "./pages/Todo.vue"
+import VueRouter from 'vue-router'
 import App from './App.vue'
-
 Vue.config.productionTip = false
-
-new Vue({
+const routes = [
+  { path: "/todo", component: Todo, name: "Todo" },
+  { path: "*", redirect: { name: "Todo" } }
+]
+const router = new VueRouter({
+  routes
+})
+export default new Vue({
+  el: '#app',
+  router,
   render: h => h(App),
 }).$mount('#app')
