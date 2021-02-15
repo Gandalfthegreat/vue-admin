5c287a3be5854a89e780813b012c487ae49dc253
wangchao11 <wangchao11@kuaishou.com>
2021-02-15 19:38:01 +0800

    feat: direction

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from "vue";
 import Todo from "./pages/Todo.vue";
+import Element from "element-ui";
 import VueRouter from "vue-router";
 import App from "./App.vue";
+import "element-ui/lib/theme-chalk/index.css";
+import ProductList from "./pages/ProductList.vue";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
+Vue.use(Element);
 const routes = [
   { path: "/todo", component: Todo, name: "Todo" },
+  { path: "/product/list", component: ProductList, name: "List" },
   { path: "*", redirect: { name: "Todo" } }
 ];
 const router = new VueRouter({
